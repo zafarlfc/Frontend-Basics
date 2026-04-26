c4532f3abbd8b531a73582b6fb0878a1eded61c3
Zafar Iqbal <zafarlfc@outlook.com>
2026-04-26 23:52:31 +0500

    Added button to add task

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { TaskService } from '../../core/services/task.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
@@ -13,19 +14,24 @@ export class TaskListComponent {
   tasks: any[] = [];
   private taskService = inject(TaskService);
 
+  newTaskTitle: string = '';
+
   ngOnInit() {
     this.tasks = this.taskService.getTasks();
   }
 
   addTask() {
+    if (this.newTaskTitle.trim() === "") return;
+
     const newTask = {
       id: Date.now(),
-      title: 'New Task',
+      title: this.newTaskTitle,
       status: 'To Do'
     };
 
     this.taskService.addTask(newTask);
     this.tasks = this.taskService.getTasks();
+    this.newTaskTitle = "";
   }
 
   statusColorMapping: Record<string, string> = {
