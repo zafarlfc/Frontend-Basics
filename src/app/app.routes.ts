d753fb91f10cacf5541c29144472b6dde185f86b
Zafar Iqbal <zafarlfc@outlook.com>
2026-05-09 00:35:23 +0500

    Added auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 import { LoginComponent } from './features/login/login.component';
 import { TaskDetailComponent } from './features/task-detail/task-detail.component';
 import { TaskFormComponent } from './features/task-form/task-form.component';
 import { TaskListComponent } from './features/task-list/task-list.component';
 
 export const routes: Routes = [
-    { path: '', component: TaskListComponent },
+    { path: '', component: TaskListComponent, canActivate: [authGuard] },
     { path: 'task/:id', component: TaskDetailComponent },
     { path: 'create', component: TaskFormComponent },
     { path: 'login', component: LoginComponent }
